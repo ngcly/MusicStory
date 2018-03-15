3edb9de3c2d26f12b77aeb937bce213f64f48be9
陈宁 <531237716@qq.com>
2018-03-15 14:43:01 +0800

    update .gitignore

---
+++ backstage/out/production/resources/static/js/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by cly on 2017/12/21 0027.
+ */
+var indexModel = (function () {
+    return {
+        initFrameHeight : function () {
+            var window_height = $(window).height();
+            var header_height = $('.layui-header').outerHeight();
+            var footer_height = $('.layui-footer').outerHeight();
+            $("#content").height(window_height - header_height - footer_height-10);
+        }
+
+    }
+})();
+
+(function () {
+    indexModel.initFrameHeight();
+})();
