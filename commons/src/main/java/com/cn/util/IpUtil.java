d6165cd284347df6f85f50ec30f853ee1d1e3580
cly00 <531237716@qq.com>
2018-06-19 14:47:06 +0800

    更改根据ip获取地址

--- commons/src/main/java/com/cn/util/IpUtil.java
+++ commons/src/main/java/com/cn/util/IpUtil.java
@@ -1,5 +1,7 @@
 package com.cn.util;
 
+import com.alibaba.fastjson.JSONObject;
+
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 
@@ -36,147 +38,16 @@ public class IpUtil {
      * @return
      * @throws UnsupportedEncodingException
      */
-    public static String getIpAddresses(String ip)
-            throws UnsupportedEncodingException {
-        // 这里调用pconline的接口
-        String urlStr = "http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip="+ip;
-        // 从http://whois.pconline.com.cn取得IP所在的省市区信息
+    public static String getIpAddresses(String ip) {
+        // 这里调用淘宝的接口
+        String urlStr = "http://ip.taobao.com/service/getIpInfo.php?ip="+ip;
         String returnStr = HttpUtil.sendHttpGet(urlStr);
-        if (returnStr != null) {
-            // 处理返回的省市区信息
-            String[] temp = returnStr.split(",");
-            if(temp.length<3){
-                return "";//无效IP，局域网测试
-            }
-            String region = (temp[4].split(":"))[1].replaceAll("\"", "");
-            System.out.println(region);
-            region = decodeUnicode(region);// 省份
-            String country = "";
-            String city = "";
-            String county = "";
-            String isp = "";
-            for (int i = 0; i < temp.length; i++) {
-                switch (i) {
-                    case 3:
-                        country = (temp[i].split(":"))[1].replaceAll("\"", "");
-                        country = decodeUnicode(country);// 国家
-                        break;
-                    case 4:
-                        region = (temp[i].split(":"))[1].replaceAll("\"", "");
-                        region =  decodeUnicode(region);// 省份
-                        break;
-                    case 5:
-                        city = (temp[i].split(":"))[1].replaceAll("\"", "");
-                        city =  decodeUnicode(city);// 市区
-                        break;
-                    case 6:
-                        county = (temp[i].split(":"))[1].replaceAll("\"", "");
-                        county =  decodeUnicode(county);// 地区
-                        break;
-                    case 7:
-                        isp = (temp[i].split(":"))[1].replaceAll("\"", "");
-                        isp = decodeUnicode(isp); // ISP公司
-                        break;
-                    default:break;
-                }
-            }
-            if(region!=null){
-                if(region.equals("新疆")){
-                    region +="维吾尔自治区" ;
-                }else if(region.equals("广西")){
-                    region +="壮族自治区" ;
-                }else if(region.equals("西藏") || region.equals("内蒙古")){
-                    region +="自治区" ;
-                }else if(region.equals("宁夏")){
-                    region +="回族自治区" ;
-                }else if(region.equals("澳门") || region.equals("香港")){
-                    region +="特别行政区" ;
-                }else if(region.equals("上海") ||region.equals("北京") ||region.equals("天津") ||region.equals("重庆")  ){
-                    region += "市";
-                }else{
-                    region +="省";
-                }
-            }
-            if(city!=null){
-                city +="市";
-            }
-            return country+region+city+county+isp;
+        JSONObject json = JSONObject.parseObject(returnStr);
+        if("0".equals(json.getString("code"))){
+            JSONObject data = json.getJSONObject("data");
+            return data.getString("country")+data.getString("area")+data.getString("region")+data.getString("city");
         }
-        return "";
-    }
-
-
-    /**
-     * unicode 转换成 中文
-     *
-     * @author fanhui 2007-3-15
-     * @param theString
-     * @return
-     */
-    public static String decodeUnicode(String theString) {
-        char aChar;
-        int len = theString.length();
-        StringBuffer outBuffer = new StringBuffer(len);
-        for (int x = 0; x < len;) {
-            aChar = theString.charAt(x++);
-            if (aChar == '\\') {
-                aChar = theString.charAt(x++);
-                if (aChar == 'u') {
-                    int value = 0;
-                    for (int i = 0; i < 4; i++) {
-                        aChar = theString.charAt(x++);
-                        switch (aChar) {
-                            case '0':
-                            case '1':
-                            case '2':
-                            case '3':
-                            case '4':
-                            case '5':
-                            case '6':
-                            case '7':
-                            case '8':
-                            case '9':
-                                value = (value << 4) + aChar - '0';
-                                break;
-                            case 'a':
-                            case 'b':
-                            case 'c':
-                            case 'd':
-                            case 'e':
-                            case 'f':
-                                value = (value << 4) + 10 + aChar - 'a';
-                                break;
-                            case 'A':
-                            case 'B':
-                            case 'C':
-                            case 'D':
-                            case 'E':
-                            case 'F':
-                                value = (value << 4) + 10 + aChar - 'A';
-                                break;
-                            default:
-                                throw new IllegalArgumentException(
-                                        "Malformed      encoding.");
-                        }
-                    }
-                    outBuffer.append((char) value);
-                } else {
-                    if (aChar == 't') {
-                        aChar = '\t';
-                    } else if (aChar == 'r') {
-                        aChar = '\r';
-                    } else if (aChar == 'n') {
-                        aChar = '\n';
-                    } else if (aChar == 'f') {
-                        aChar = '\f';
-                    }
-                    outBuffer.append(aChar);
-                }
-            } else {
-                outBuffer.append(aChar);
-            }
-        }
-        return outBuffer.toString();
+        return null;
     }
 
 }
