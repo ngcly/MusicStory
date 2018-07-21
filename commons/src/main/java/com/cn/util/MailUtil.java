3d6006f23da0859ad0b3908c69fc317c3218d190
cly00 <531237716@qq.com>
2018-07-21 11:47:11 +0800

    修改部分配置文件

---
+++ commons/src/main/java/com/cn/util/MailUtil.java
@@ -0,0 +1,60 @@
+package com.cn.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 描述:
+ * 邮件工具类
+ *
+ * @author chen
+ * @create 2018-07-21 11:22
+ */
+@Component
+public class MailUtil {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${mail.from}")
+    private String from;
+
+    /**
+     * 发送简单邮件
+     * @param to
+     * @param subject
+     * @param content
+     */
+    public void sendSimpleMail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        mailSender.send(message);
+    }
+
+    /**
+     * 发送html格式邮件
+     * @param to
+     * @param subject
+     * @param content
+     */
+    public void sendHtmlMail(String to, String subject, String content) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        //true表示需要创建一个multipart message
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(from);
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(content, true);
+        mailSender.send(message);
+    }
+
+}
