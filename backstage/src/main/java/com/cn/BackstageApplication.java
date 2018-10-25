921744f5e32afa0c0779e662dea207af9c0c1b6b
ngcly <531237716@qq.com>
2018-10-25 17:29:54 +0800

    添加ssl证书

--- backstage/src/main/java/com/cn/BackstageApplication.java
+++ backstage/src/main/java/com/cn/BackstageApplication.java
@@ -1,7 +1,13 @@
 package com.cn;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 后台主启动类
@@ -18,4 +24,31 @@ public class BackstageApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackstageApplication.class, args);
     }
+
+//    @Bean
+//    public Connector connector(){
+//        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(80);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
+//
+//    @Bean
+//    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+//        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint=new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection=new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(connector);
+//        return tomcat;
+//    }
 }
