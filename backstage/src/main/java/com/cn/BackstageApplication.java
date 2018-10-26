ecdc9c4ba08801a45b570a25f8883f5bd6f9385d
ngcly <531237716@qq.com>
2018-10-26 11:47:40 +0800

    添加ssl证书

--- backstage/src/main/java/com/cn/BackstageApplication.java
+++ backstage/src/main/java/com/cn/BackstageApplication.java
@@ -1,12 +1,10 @@
 package com.cn;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -25,30 +23,21 @@ public class BackstageApplication {
         SpringApplication.run(BackstageApplication.class, args);
     }
 
-//    @Bean
-//    public Connector connector(){
-//        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(80);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(443);
-//        return connector;
-//    }
-//
-//    @Bean
-//    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
-//        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint=new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection=new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//        tomcat.addAdditionalTomcatConnectors(connector);
-//        return tomcat;
-//    }
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHTTPConnector());
+        return tomcat;
+    }
+
+    private Connector createHTTPConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        //同时启用http（80）、https（443）两个端口
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(80);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+
 }
