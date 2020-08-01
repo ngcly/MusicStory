5f75b8424b7f151c6e9466dea9643aec86bee4b3
chenning <531237716@qq.com>
2020-08-01 16:49:31 +0800

    版本升级

--- interface/src/main/java/com/cn/config/SwaggerConfig.java
+++ interface/src/main/java/com/cn/config/SwaggerConfig.java
@@ -11,7 +11,7 @@ import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import java.security.Principal;
 import java.util.Arrays;
@@ -20,7 +20,7 @@ import java.util.Arrays;
  * @author ngcly
  */
 @Configuration
-@EnableSwagger2
+@EnableSwagger2WebMvc
 public class SwaggerConfig {
 
     @Bean
