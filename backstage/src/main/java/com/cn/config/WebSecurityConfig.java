748e55a1119f15d08dc808b2dd784700f20a28dc
ngcly <531237716@qq.com>
2021-09-15 19:30:31 +0800

    修复后台注入错误

--- backstage/src/main/java/com/cn/config/WebSecurityConfig.java
+++ backstage/src/main/java/com/cn/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.cn.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -111,4 +112,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new LoginFailureHandler();
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
