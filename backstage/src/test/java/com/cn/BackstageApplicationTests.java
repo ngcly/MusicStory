6e10f8c6093fdc9d1861346e2a153117d6388d51
chenning <ning@Youland.com>
2021-09-27 17:04:42 +0800

    添加方法

--- backstage/src/test/java/com/cn/BackstageApplicationTests.java
+++ backstage/src/test/java/com/cn/BackstageApplicationTests.java
@@ -1,32 +1,19 @@
 package com.cn;
 
-import com.cn.enums.ConfigEnum;
-import org.jasypt.encryption.StringEncryptor;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * 测试类
  *
- * @author chen
+ * @author ngcly
  * @date 2018-03-15 12:46
  */
 @SpringBootTest
 public class BackstageApplicationTests {
-    @Autowired
-    private StringEncryptor stringEncryptor;
-
-    @BeforeAll
-    public static void setupClass() {
-        ConfigEnum configEnum = ConfigEnum.JASYPT_ENCRYPTOR;
-        System.setProperty(configEnum.getKey(),configEnum.getValue());
-    }
-
 
     @Test
     public void contextLoads() {
-        System.out.println("Key: "+stringEncryptor.decrypt("BFmGqMfhERt8BVG81BuQFAI4L165TxyjahwvlTGx9QnjkKAWytNvhhtfiajw1mhsGTLh5JErMOFrjltogIkOUg=="));
+
     }
 }
