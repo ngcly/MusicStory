b34334fa82d441c14220cc5937215536f6fd6bc4
ngcly <531237716@qq.com>
2021-11-18 21:13:04 +0800

    版本升级

--- backstage/src/main/java/com/cn/BackstageApplication.java
+++ backstage/src/main/java/com/cn/BackstageApplication.java
@@ -8,8 +8,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 /**
  * 后台主启动类
  *
- * @author chen
- * @date 2017-12-30 15:51
+ * @author ngcly
+ * @since 2017-12-30 15:51
  */
 //springboot 启动类不能直接放在 java 文件下 不然会报错
 //springboot 扫描包默认是以启动类所在的包 从上往下扫描
