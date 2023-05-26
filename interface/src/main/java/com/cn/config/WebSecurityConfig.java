01214382cde01403a111adefa5a581d7a326fe5c
chenning <ning@Youland.com>
2023-05-26 16:57:02 +0800

    升级springboot

--- interface/src/main/java/com/cn/config/WebSecurityConfig.java
+++ interface/src/main/java/com/cn/config/WebSecurityConfig.java
@@ -66,7 +66,7 @@ public class WebSecurityConfig {
                 .authenticationProvider(new MyAuthenticationProvider(userService))
                 .userDetailsService(userService)
                 .passwordEncoder(passwordEncoder)
-                .init()
+                .and()
                 .build();
     }
 
