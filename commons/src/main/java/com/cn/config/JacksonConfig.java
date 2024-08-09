5fa7f2f082868932406692d69841c71d74a2ecf9
LinChen <ning@Youland.com>
2024-08-09 15:10:20 +0800

    去掉 JacksonConfig.java

--- commons/src/main/java/com/cn/config/JacksonConfig.java
+++ commons/src/main/java/com/cn/config/JacksonConfig.java
@@ -18,24 +18,24 @@ import java.time.format.DateTimeFormatter;
  * jackson 序列化与反序列化 时间类型
  * @author ngcly
  */
-@Configuration
-public class JacksonConfig {
-
-    @Value("${spring.jackson.date-format}")
-    private String pattern;
-
-    @Bean
-    public ObjectMapper serializingObjectMapper() {
-        JavaTimeModule javaTimeModule = new JavaTimeModule();
-        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern)));
-        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
-        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ISO_LOCAL_TIME));
-        javaTimeModule.addSerializer(Instant.class, InstantSerializer.INSTANCE);
-
-        return new ObjectMapper()
-                .registerModule(new ParameterNamesModule())
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .registerModule(javaTimeModule);
-    }
-
-}
+//@Configuration
+//public class JacksonConfig {
+//
+//    @Value("${spring.jackson.date-format}")
+//    private String pattern;
+//
+//    @Bean
+//    public ObjectMapper serializingObjectMapper() {
+//        JavaTimeModule javaTimeModule = new JavaTimeModule();
+//        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(pattern)));
+//        javaTimeModule.addSerializer(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ISO_LOCAL_DATE));
+//        javaTimeModule.addSerializer(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ISO_LOCAL_TIME));
+//        javaTimeModule.addSerializer(Instant.class, InstantSerializer.INSTANCE);
+//
+//        return new ObjectMapper()
+//                .registerModule(new ParameterNamesModule())
+//                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+//                .registerModule(javaTimeModule);
+//    }
+//
+//}
